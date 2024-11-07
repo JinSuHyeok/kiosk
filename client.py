162ea673c8b75ec8ed42eb7363ac73f90ee4f6d4
JinSuHyeok <jinshy0112@naver.com>
2024-11-07 21:47:32 +0900

    Client.py Updated

--- client.py
+++ client.py
@@ -1,5 +1,11 @@
 import requests
+# pip install requets
 
-d = input("enter a direction, left or right: ")
+while True:
+    # 원래 소켓으로 조이스틱으로부터 데이터를 받아오는데, 지금은 테스트이므로 표준입력으로 대체 
 
-resp = requests.get(f"http://localhost:8000/direction/{d}")
+    # 여기서 방향 받아옴, 실제로는 조이스틱에서 받아온 데이터로 대체 
+    d = input("enter a direction, left or right: ")
+
+    # 백엔드 서버로 요청 날리기 
+    resp = requests.get(f"http://localhost:8000/direction/{d}")
